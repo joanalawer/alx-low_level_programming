880823e864c1b3971c47599e9e5c147880ac8487
joanalawer <lawerjoana477@gmail.com>
2023-02-16 23:03:12 +0000

    Add a/an to output string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %lu bytes(s)\n", sizeof(char));
-	printf("Size of int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of long int: %lu bytes(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 	return (0);
 }
