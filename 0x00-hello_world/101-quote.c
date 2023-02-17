1707511304fe92f9a347199a59eb98a682f316ff
joanalawer <lawerjoana477@gmail.com>
2023-02-17 04:38:28 +0000

    Edit Betty coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * Description: Program prints a line of text to standard error
  * Return: Always 1 (Success)
  */
-int main(void){
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
