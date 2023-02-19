b550851705d2848c04cc53840c75b48c9e7d3233
joanalawer <lawerjoana477@gmail.com>
2023-02-19 21:57:55 +0000

    Print the lowercase alphabet in reverse. Use only putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print lowercase alphabet in reverse. Using only putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alphabets;
+
+        for (alphabets = "z"; alphabets >=; --alphabets)
+        {
+                putchar(alphabets);
+        }
+	putchar('\n');
+
+                return (0);
