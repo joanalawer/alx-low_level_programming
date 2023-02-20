a7ef13f150532cc672076333d308354859abddc4
joanalawer <lawerjoana477@gmail.com>
2023-02-20 17:57:12 +0000

    Check indentation for betty coding style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: Print single digit numbers of base 10, from 0. Use only putchar.
+ * Description: Print single digit numbers of base 10, from 0. Use only putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int i;
-
-        for (i = 0; i < 10; ++i)
-        {
-                putchar((i % 10) + '0');
-        }
+	int i;
+	
+	for (i = 0; i < 10; ++i)
+	{
+		putchar((i % 10) + '0');
+	}
 	putchar('\n');
-
-                return (0);
+	
+	return (0);
 }
