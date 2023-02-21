4a33ab7b486f67ab8d82f88d82012a3168e97908
joanalawer <lawerjoana477@gmail.com>
2023-02-21 21:40:11 +0000

    Add ; to end of statements

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,8 +3,13 @@
  * Description: Program prints _putchar, followed by a new line.
  * Return: On success (0)
  */
-int _putchar()
+int main(void)
 {
-	_putchar("putchar%c\n")
+	char s = "_putchar";
+	
+	_putchar("%c", s);
+	
+	_putchar('\n');
+
 	return (0);
 }
