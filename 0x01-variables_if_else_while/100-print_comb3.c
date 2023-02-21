74713e7e165df2dbdd3ccf3dd6f57b931117c925
joanalawer <lawerjoana477@gmail.com>
2023-02-21 01:42:06 +0000

    Recheck betty codding style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,7 @@ int main(void)
 		for (y = x + 1; y < 10; y++)
 		{
 			putchar((x % 10) + '0');
-			putchar((y % 10) + '0');
-			
+			putchar((y % 10) + '0');			
 			if (x == 8 && y == 9)
 				continue;
 
