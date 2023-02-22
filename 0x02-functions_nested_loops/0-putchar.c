0ecb9cc725a7a71daedd60fb6b7d168f32790b72
joanalawer <lawerjoana477@gmail.com>
2023-02-22 14:00:11 +0000

    Debug task 0 by adding main.h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - Entry pooint
  * Description: Program prints _putchar, followed by a new line.
  * Return: On success (0)
  */
-int _putchar(char c)
+int main(void)
 {
 	char i;
 
