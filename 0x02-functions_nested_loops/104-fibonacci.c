9014e759ad3594a1707d23de1fe6783a4a182e9a
joanalawer <lawerjoana477@gmail.com>
2023-02-22 17:58:05 +0000

    Changed value of b and clear whitespcaes

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,16 +9,17 @@ int main(void)
 	int i;
 	long int a;
 	long int b;
-	
+
 	a = 1;
-	b = 1;
+	b = 2;
 	i = 2;
 
 	printf("%ld, %ld, ", a, b);
-	
+
 	while (i < 98)
 	{
 		long int temp = b;
+
 		b += a;
 		a = temp;
 		i++;
