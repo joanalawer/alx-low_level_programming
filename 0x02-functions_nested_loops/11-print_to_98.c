52c7676bed016d4ba193ad9af94775315be3f63b
joanalawer <lawerjoana477@gmail.com>
2023-02-22 15:12:43 +0000

    Change print command

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,11 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		_putchar("%d\n", n);
+		printf("%d\n", n);
 	}
 	else
 	{
-		_putchar("%d, ", n);
+		printf("%d, ", n);
 		print_to_98((n < 98) ? n + 1 : n - 1);
 	}
 }
