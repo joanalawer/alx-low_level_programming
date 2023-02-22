d6a5bd2e8a1f259a017a2da42d683c4c5bf8314d
joanalawer <lawerjoana477@gmail.com>
2023-02-22 16:21:40 +0000

    Program performs some operations on multiples of 3 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program prints the natural numbers below 1024 that are multiplies of 3 or 5
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			n += i;
+	}
+
+	printf("%d\n", n);
+
+	return (0);
+}
