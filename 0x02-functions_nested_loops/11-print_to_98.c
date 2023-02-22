7f9761fc2f5df21d338e5882cdb3882d74c76031
joanalawer <lawerjoana477@gmail.com>
2023-02-22 14:58:48 +0000

    Debug logic statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n: first number passed to function to be printed
- * Return: Always 0 (Success)
+ * @n: Input number
+ * Return: void
  */
 void print_to_98(int n)
 {
 	int i;
 
-	for(i = n; i <= 98; i++)
+	for(i == n; i <= 98; i++)
 	{
 		_putchar(i);
 		_putchar(", ");
 	}
 	_putchar('\n');
-
-	return (0);
 }
