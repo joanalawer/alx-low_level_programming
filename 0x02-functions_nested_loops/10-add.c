06a5b74a9e544b62b87072a834864868cedf9c56
joanalawer <lawerjoana477@gmail.com>
2023-02-22 14:14:59 +0000

    Program adds two integers and prints out result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add(int, int) - Adds two integers and returns the result
+ * Return: Resulting sum
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	_putchar(sum);
+}
