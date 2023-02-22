dcdeffa7160f1f2343885163d7d5d93b09a7db9e
joanalawer <lawerjoana477@gmail.com>
2023-02-22 14:20:49 +0000

    Add return statement

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  * add(int, int) - Adds two integers and returns the result
- * Return: Resulting sum
+ * Return: Always 0 (Success)
  */
 int add(int a, int b)
 {
 	int sum;
+
 	sum = a + b;
 	_putchar(sum);
+	
+	return (0);
 }
