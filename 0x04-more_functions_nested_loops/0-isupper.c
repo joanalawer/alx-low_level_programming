434fcdedf6274485069c081f692f3e52a61b7c08
joanalawer <lawerjoana477@gmail.com>
2023-02-23 20:35:20 +0000

    Created program to check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Function checks for uppercase character
+ * Return: 1 if c ic uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+	char ch = 'A';
+
+	if (_isupper(ch))
+		return(1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+/**
+ * Header file: Contains the prototypes of all functions and 
+ * the prototype of the function _putchar
+ */
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
