5e48f1c811da67c4854988850c13788f46f4a82b
joanalawer <lawerjoana477@gmail.com>
2023-02-23 22:17:30 +0000

    prints the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 t0 9, followed by newline.
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+
+	return (void);
+}
