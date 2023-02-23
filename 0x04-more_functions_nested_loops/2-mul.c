f324d84112b3894c3a287c6d81885a3561bbb772
joanalawer <lawerjoana477@gmail.com>
2023-02-23 22:07:08 +0000

    Created function to multiply two integers, updated main header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a: Integer to be multiplied
+ * @b: Integer to be multiplied
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
