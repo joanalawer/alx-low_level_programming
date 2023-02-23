8112125088d5509a12b06e360616c08fdb4a8282
joanalawer <lawerjoana477@gmail.com>
2023-02-23 20:38:52 +0000

    Added description for c and space before parenthsis

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isupper - Function checks for uppercase character
+ * @c: Input letter
  * Return: 1 if c ic uppercase, 0 otherwise
  */
 int _isupper(int c);
@@ -8,7 +9,7 @@ int _isupper(int c);
 	char ch = 'A';
 
 	if (_isupper(ch))
-		return(1);
+		return (1);
 	else
 		return (0);
 }
