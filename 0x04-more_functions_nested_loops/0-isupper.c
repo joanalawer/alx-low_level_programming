43ae186de3d79128f666ec8c102f68d99835b1cb
joanalawer <lawerjoana477@gmail.com>
2023-02-23 21:27:35 +0000

    Created function to check for digits from 0 through to 9

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,10 @@
  * @c: Input letter
  * Return: 1 if c if uppercase, 0 otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
