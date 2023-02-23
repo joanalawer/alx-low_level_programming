eb8f20c5d784fb4d0dbd7e14e808ec909f062847
joanalawer <lawerjoana477@gmail.com>
2023-02-23 20:51:44 +0000

    Changed characters to ASCII values

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 /**
  * _isupper - Function checks for uppercase character
  * @c: Input letter
- * Return: 1 if c ic uppercase, 0 otherwise
+ * Return: 1 if c if uppercase, 0 otherwise
  */
 int _isupper(int c);
 {
 
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
