3ad023c6bac441887643edc52fd0bdaa2aaf0d25
joanalawer <lawerjoana477@gmail.com>
2023-02-23 23:32:09 +0000

    Print 0 to 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,14 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (j = 0; j <= 9; ++j)
-		for (i = 0; i <= 14; ++i)
+	for (j = 0; j < 10; ++j)
+	{
+		for (i = 0; i < 15; ++i)
+		{
 			if (i > 9)
 				_putchar('0' + i / 10);
 			_putchar('0' + i % 10);
+		}
 		_putchar('\n');
+	}
 }
