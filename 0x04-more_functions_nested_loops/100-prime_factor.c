dafc27a791eda36e3eecacb45edd9fd6bcc89064
joanalawer <lawerjoana477@gmail.com>
2023-02-24 17:40:53 +0000

    Punsigned long fac = 2;

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program finds and prints the largest prime factor 
+ * of the number 612852475143
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long factor = 2;
+	unsigned long num = 612852475143;
+
+	while (num != 1)
+	{
+		if (num % factor == 0)
+			num /= factor;
+		else
+			factor++;
+	}
+	printf("%lu\n", factor);
+	return (0);
+}
