0b7545d2d74a074c327fa9a210937e7e0d0ddef0
joanalawer <lawerjoana477@gmail.com>
2023-02-27 21:01:10 +0000

    Cretaed pointer to update value of a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * void reset_to_98(int *n) - takes a pointer to an int as parameter and 
+ * updates the value it points to to 98.
+ * @n: pointer to update value of n
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
