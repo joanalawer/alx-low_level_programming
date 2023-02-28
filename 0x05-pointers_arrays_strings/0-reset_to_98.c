313c7158d1af24648183dd2ddf729912ea21774d
joanalawer <lawerjoana477@gmail.com>
2023-02-28 04:55:41 +0000

    Removed void from description of prototype

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * void reset_to_98 - takes a pointer to an int as parameter and
+ * reset_to_98 - takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: pointer to update value of n
  * Return: void
