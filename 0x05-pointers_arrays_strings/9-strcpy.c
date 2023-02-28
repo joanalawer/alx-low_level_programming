f32a283b8b35be5c9af90ec23a63962dd6966adf
joanalawer <lawerjoana477@gmail.com>
2023-02-28 05:15:05 +0000

    Checked betty coding style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strcpy - Copies string from src to buffer pointed by dest
- * dest: Where string is copied to
- * src: Where string is copied from
+ * @dest: Where string is copied to
+ * @src: Where string is copied from
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
