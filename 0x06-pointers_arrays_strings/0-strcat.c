efb401f3b2348427fdb2d754041161a159926654
joanalawer <lawerjoana477@gmail.com>
2023-03-01 17:28:58 +0000

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main_h"
+/**
+ * *_strca t- concatennates two strings
+ * @dest:destination string
+ * @src: source string
+ * Return: pointer ro reulting dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	*_strcat(dest, src);
+
+	return (dest);
+}
