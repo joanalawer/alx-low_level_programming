e44f87ba1dc00671f9dfe4a95f145cd63aac6da6
joanalawer <lawerjoana477@gmail.com>
2023-03-01 18:50:16 +0000

    Included header and documentation in file

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: defines the array
+ * @n: number of elements of the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+
+}
