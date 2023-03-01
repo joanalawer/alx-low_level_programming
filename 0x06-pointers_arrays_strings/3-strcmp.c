46a4e13e4cc468f62cc6232ffb00cfc6bb31cc1c
joanalawer <lawerjoana477@gmail.com>
2023-03-01 18:43:26 +0000

    Included header and documentation in file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Integer value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int ret;
+
+
+	return(0);
+}
