b573addb82ea13524a2cd1a15c427be744933880
joanalawer <lawerjoana477@gmail.com>
2023-03-02 20:06:57 +0000

    Attempt at task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * _strcat - concatenates two strings
  * @dest:destination string
  * @src: source string
- * Return: pointer ro reulting dest
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
