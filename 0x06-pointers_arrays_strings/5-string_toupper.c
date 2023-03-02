a5cd59707a9e57cd32b121936f4defb9dc21eec6
joanalawer <lawerjoana477@gmail.com>
2023-03-02 20:29:04 +0000

    Changes all lowercase to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,5 +5,17 @@
  */
 char *string_toupper(char *)
 {
+	char s;
+	int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
 }
