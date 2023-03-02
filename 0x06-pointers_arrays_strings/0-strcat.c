afa0fa1e1e6a9371d83619363fdf4c87dc06ae17
joanalawer <lawerjoana477@gmail.com>
2023-03-02 18:20:39 +0000

    Another attempt at function

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	*_strcat(dest, src);
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dst[i] != 0)
+	{
+		i++;
+	}
+	while (src[j] != 0)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+
+	dest[i + j] = 0;
 
 	return (dest);
 }
