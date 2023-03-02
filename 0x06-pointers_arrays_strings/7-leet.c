c80ed772ee9f7aa34c31f4848edab7319d8b76a8
joanalawer <lawerjoana477@gmail.com>
2023-03-02 21:55:29 +0000

    Replaces letters with numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - entry point
- * Description: Encodes a string into 1337
- * Return: 0 (success)
+ * leet - replaces letters with numbers
+ * @s: string of letters
+ * Return: pointer to replaced letters
  */
-int main(void)
+char *leet(char *s)
 {
+	int i, j;
+
+	char letters[80] = "aAeEoOtTlL";
+	char numbers[80] = "43071";
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j / 2];
+			}
+		}
+	}
+	return (s);
 }
