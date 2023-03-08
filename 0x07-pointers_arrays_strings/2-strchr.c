046362f1c0b5ad388f7464a7651357ab1c3f599d
joanalawer <lawerjoana477@gmail.com>
2023-03-08 15:18:46 +0000

    Set index of character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,15 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		if (*s == c)
+		if (s[0] == c)
 		{
 			return (s);
 		}
 		s++;
 	}
-	if (*s == c)
+	if (s[1] == c)
 	{
-		return (s);
+		return (s + 1);
 	}
 
 	return (NULL);
