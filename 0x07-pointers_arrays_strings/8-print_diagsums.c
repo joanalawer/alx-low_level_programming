609ca453cb7d5f7e180d89e00ea7edc75b99d3fa
joanalawer <lawerjoana477@gmail.com>
2023-03-13 11:25:16 +0000

    Restructure code with second counter in for loop

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i,j;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[(size + 1) * i];
-		sum2 += a[(size - 1) * (b + 1)];
+	}
+	for (j = 0; j < size; j++)
+	{
+		sum2 += a[(size - 1) * (j + 1)];
 	}
 
 	printf("%d, %d\n", sum1, sum2);
