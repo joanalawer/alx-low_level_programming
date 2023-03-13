339ab98961dfc35288acb1f5803611206c8f04a8
joanalawer <lawerjoana477@gmail.com>
2023-03-13 10:50:10 +0000

    Correct operation formular in sum1

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,12 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1 = 0, sum2 = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i * (size + i)];
+		sum1 += a[i * (size + 1)];
 		sum2 += a[(size - 1) * (i + 1)];
 	}
 	printf("%d", sum1);
