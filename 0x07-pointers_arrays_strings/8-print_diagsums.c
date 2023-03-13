1dd3c134456587058b7c868edc6d55afb4a116c0
joanalawer <lawerjoana477@gmail.com>
2023-03-13 10:31:49 +0000

    Correct operation statements

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints the sum of two diagonals of a square matrix
  * @a: matrix of integers
@@ -12,8 +13,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i * size + i];
-		sum2 += a[i * size + (size - i - 1)];
+		sum1 += a[i * (size + i)];
+		sum2 += a[i * (size + (size - i - 1))];
 	}
 	_putchar(sum1);
 	_putchar(sum2);
