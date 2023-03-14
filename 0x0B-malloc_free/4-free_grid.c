d79c80e68bf0028f8649ae98cebdad869caca767
joanalawer <lawerjoana477@gmail.com>
2023-03-14 08:40:22 +0000

    Return NULL

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,8 +12,13 @@ void free_grid(int **grid, int height)
 	int i;
 
 	if (grid == NULL || height <= 0)
-		return;
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
+	{
 		free(grid[(height - 1) - i]);
+	}
+
 	free(grid);
 }
