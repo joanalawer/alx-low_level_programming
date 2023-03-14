af0e17ecffb0acd0e4ddbdc6756eee75c61c912c
joanalawer <lawerjoana477@gmail.com>
2023-03-14 08:44:02 +0000

    Comment first loop

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,10 +11,12 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid == NULL || height <= 0)
+	/**
+	 * if (grid == NULL || height <= 0)
 	{
 		return (NULL);
 	}
+	*/
 	for (i = 0; i < height; i++)
 	{
 		free(grid[(height - 1) - i]);
