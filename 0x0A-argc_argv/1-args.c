939c76553a864f777a61b900e70cd35a77f1cd5a
joanalawer <lawerjoana477@gmail.com>
2023-03-16 19:25:45 +0000

    Marked unused arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: prints char of arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: prints number of arguments
+ * @argv: print characters of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
