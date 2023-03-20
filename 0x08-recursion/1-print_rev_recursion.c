ced57c52dbfbc3b02d47bd3be792e2d3b1dae499
joanalawer <lawerjoana477@gmail.com>
2023-03-20 19:54:27 +0000

    Checked betty style coding and spellings

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _print_rev__recursion - prints a string
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to string
  * Return: 0
  */
-void _print_rev__recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                _print_rev_recursion(++s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(++s);
 		_putchar(*s);
-        }
+	}
 }
