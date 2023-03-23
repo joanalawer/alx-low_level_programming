99051bc5891923cd1ea1c431304553b677440a55
joanalawer <lawerjoana477@gmail.com>
2023-03-23 17:09:34 +0000

    Rewrite task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,46 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * argstostr - function concatenates all the arguments of your program.
+ * argstostr - function concatenates all the arguments of program.
  * @ac: args count
  * @av: arguments
  * Return: pointer to string, NULL on failure
  */
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int i, j, n, len;
+	char *str, *temp;
+	int i, j, len;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			len += j + 1;
-		}
-	}
-
-	str = malloc(len + 1);
-	if (str == 0)
-	{
-		return (NULL);
-	}
-
-	n = 0;
+	len = 0;
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; k++)
+		temp = *(av + i);
+		for (j = 0; temp[j]; j++)
 		{
-			str[n] = av[i][j];
-			n++;
+			len++;
 		}
-		str[n] = '\n';
-		n++;
+
+		len += 1;
 	}
+
+	str = malloc(sizeof(char) * len + 1);
+	len = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		temp = *(av + i);
+
+		for (j = 0; temp[j]; ++j)
+		{
+			str[len] = temp[j];
+			len++;
+		}
+			str[len] = '\n';
+			len++;
+	}
+	str[len] = '\0';
 	return (str);
 }
