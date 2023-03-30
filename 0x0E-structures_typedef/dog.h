fb689fc2a608aea8eab30c644e8f64096662082a
joanalawer <lawerjoana477@gmail.com>
2023-03-30 23:02:30 +0000

    Added prototype for functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
