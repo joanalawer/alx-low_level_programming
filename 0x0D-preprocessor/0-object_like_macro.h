3998f57f79002b6eb3812721c0f09683b74129dc
joanalawer <lawerjoana477@gmail.com>
2023-03-31 16:07:18 +0000

    Added description

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,11 @@
 #ifndef object_like_macro_h
 #define object_like_macro_h
 
+/*
+ *  Description: Header file that defines a macro named
+ *  SIZE as an abbreviation for the token 1024.
+ */
+
 #define SIZE 1024
 
 #endif
