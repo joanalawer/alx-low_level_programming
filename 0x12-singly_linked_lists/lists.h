76581480e9c78505e26cb3de22f09f6106081d7b
joanalawer <lawerjoana477@gmail.com>
2023-03-31 00:26:48 +0000

    Added standard library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef lists_h
 #define lists_h
+
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: pointerstring
@@ -18,4 +20,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
