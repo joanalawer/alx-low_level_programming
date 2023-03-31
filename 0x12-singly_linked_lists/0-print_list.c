b93f07544a2099f845a78180d615e39abb25a95f
joanalawer <lawerjoana477@gmail.com>
2023-03-31 00:33:09 +0000

    Added standard library

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,9 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list - prints all the elements of a list_t list
  * @h: the list
- * Return: number of nodes in h.
+ * Return: number of nodes in h
  */
 
 size_t print_list(const list_t *h)
@@ -12,14 +12,18 @@ size_t print_list(const list_t *h)
 	size_t nodes = 0;
 
 	while (h)
+	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-
+		}
 		nodes++;
 		h = h->next;
+	}
 
 	return (nodes);
 }
