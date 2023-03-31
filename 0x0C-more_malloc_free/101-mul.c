235eaf2f4321b2cea3be30c98bc16265f019de0c
joanalawer <lawerjoana477@gmail.com>
2023-03-31 15:28:54 +0000

    Another attempt at solution

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
+ *
  * Return: The length of the string.
  */
 int find_len(char *str)
@@ -18,8 +18,12 @@ int find_len(char *str)
 }
 
 /**
- * create_xarray - Creates an array of chars and initializes it with the character 'x'.
+ * create_xarray - Creates an array of chars and initializes it with
+ *                 the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
+ *
+ * Description: If there is insufficient space, the
+ *              function exits with a status of 98.
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
@@ -44,6 +48,7 @@ char *create_xarray(int size)
  * iterate_zeroes - Iterates through a string of numbers containing
  *                  leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
+ *
  * Return: A pointer to the next non-zero element.
  */
 char *iterate_zeroes(char *str)
@@ -57,6 +62,9 @@ char *iterate_zeroes(char *str)
 /**
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
+ *
+ * Description: If c is a non-digit, the function
+ *              exits with a status of 98.
  * Return: The converted int.
  */
 int get_digit(char c)
@@ -78,6 +86,9 @@ int get_digit(char c)
  * @mult: The string of numbers.
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
+ *
+ * Description: If mult contains a non-digit, the function
+ *              exits with a status value of 98.
  */
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
@@ -164,6 +175,9 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * main - Multiplies two positive numbers.
  * @argv: The number of arguments passed to the program.
  * @argc: An array of pointers to the arguments.
+ *
+ * Description: If the number of arguments is incorrect or one number
+ *              contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
