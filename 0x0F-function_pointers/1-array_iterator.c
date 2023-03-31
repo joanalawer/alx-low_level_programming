29b052e45e2cef3f54e7b3334768e7d61c88b715
joanalawer <lawerjoana477@gmail.com>
2023-03-31 19:08:51 +0000

    Correct header

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
-#include "function_pointers,h"
-/*
+#include "function_pointers.h"
+/**
  * array_iterator - function that executes a function given as
  * a parameter on each element of an array.
  * @array: The array.
