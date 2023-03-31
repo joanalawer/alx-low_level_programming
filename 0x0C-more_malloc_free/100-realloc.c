5ed6c022843908bdde27315f0102228da33bb2f5
joanalawer <lawerjoana477@gmail.com>
2023-03-31 15:00:55 +0000

    Another attempt at task solution

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,47 +10,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *mem;
-	char *ptr_copy, *f;
+	char *nptr;
 	unsigned int i;
 
 	if (new_size == old_size)
-	{
 		return (ptr);
-	}
+
 	if (ptr == NULL)
 	{
-		mem = malloc(new_size);
+		nptr = malloc(new_size);
 
-		if (mem == NULL)
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
 		{
+			free(ptr);
 			return (NULL);
 		}
-		return (mem);
 	}
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
 		return (NULL);
-	}
-
-	ptr_copy = ptr;
-	mem = malloc(sizeof(*ptr_copy) * new_size);
-
-	if (mem == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	f = mem;
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		f[i] = *ptr_copy++;
+		nptr[i] = ((char *) ptr)[i];
 	}
 
 	free(ptr);
-	return (mem);
+	return (nptr);
 }
