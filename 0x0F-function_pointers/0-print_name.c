a8b0daf39781f65ceeb2fee118148da6dcb7be66
joanalawer <lawerjoana477@gmail.com>
2023-03-31 17:56:34 +0000

    Added return in docs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,7 @@
  * print_name - function prints a name.
  * @name: name to be printed.
  * @f: pointer to a function that prints a name.
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
