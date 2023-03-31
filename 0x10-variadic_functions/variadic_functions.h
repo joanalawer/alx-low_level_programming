88fdea32e6bd67a0cc482d729aea07fba746c529
joanalawer <lawerjoana477@gmail.com>
2023-03-31 20:42:41 +0000

    Function prints anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -20,4 +20,11 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/* Prototypes for task 3 */
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...)
+
 #endif
