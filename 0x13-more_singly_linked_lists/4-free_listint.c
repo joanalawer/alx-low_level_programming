b68c8efaf3fd01039ed7bbcf8161f04b8fedf273
joanalawer <lawerjoana477@gmail.com>
2023-04-05 00:45:25 +0000

    Assign node value

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,21 +4,26 @@
 
 /**
  * free_listint - Function frees a listint_t list.
- * @head: Pointer to node
+ * @head: Pointer to first node
  * Return: NULL
  */
 void free_listint(listint_t *head)
 {
 	/* Declare a temporal node for list */
 	listint_t *temp = malloc(sizeof(listint_t));
+	/* Assign temp node to head */
+	temp = head;
 
 	while (head != NULL)
 	{
 		temp = head->next;
-		
+
 		free(head);
 
 		head = temp;
-
 	}
+
+	head = NULL;
+
+	return;
 }
