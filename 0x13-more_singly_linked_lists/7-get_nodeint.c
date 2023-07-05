5d8dc53a872eb0d582ff80e182d282ce08813054
joanalawer <lawerjoana477@gmail.com>
2023-07-05 13:55:58 +0000

    Variable name

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	/* Declare node for head */
 	listint_t *temp = head;
-
 	unsigned int = 0;
 
-	while (current != NULL)
+	while (temp != NULL)
 	{
 		if (count == index)
 		{
