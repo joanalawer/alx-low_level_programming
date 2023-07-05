75e32db43bcc08592c0ff4709f90f30531a90c07
joanalawer <lawerjoana477@gmail.com>
2023-07-05 14:02:12 +0000

    Set conditon for loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int count = 0;
 
-	while (temp && count < NULL)
+	while (temp && count < index)
 	{
 		temp = temp->next;
 		count++;
