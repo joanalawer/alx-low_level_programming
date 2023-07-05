467c9996bd8a3366e9ad633a1db43998ad4f0d40
joanalawer <lawerjoana477@gmail.com>
2023-07-05 13:53:53 +0000

    Check betty style code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,11 +19,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (count == index)
 		{
-			return temp;
+			return (temp);
 		}
 		count++;
 		temp = temp->next;
 	}
 
-	return NULL;
+	return (NULL);
 }
