042b5fad2d9a1238f937b24bc4ba30f1d226a5f1
joanalawer <lawerjoana477@gmail.com>
2023-07-07 03:53:22 +0000

    Add standard library header

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,24 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - Function prints the binary equivalent of a decimal number.
- * @n: Number to print in binary
+ * print_binary - Function prints the binary representation of a number.
+ * @n: Binary representation
+ * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int num;
-	int dec_num, count = 0;
-
-	for (dec_num = 63; dec_num >= 0; dec_num--)
+	if (n == 0)
 	{
-		num = n >> dec_num;
-
-		if (num & 1)
-		{
-			_putchar('1');
-			count++;
-		}
-		else if (count)
-		{
-			_putchar('0');
-		}
+		putchar('0');
+		return;
 	}
-
-	if (!count)
+	else if (n == 1)
 	{
-		_putchar('0');
+		putchar ('1');
+		return;
 	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
